143ed20ec12b99f36ff86a209ce010065960e428
boardguy1024 <boardguy1024@gmail.com>
2022-08-02 23:12:14 +0900

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by park kyung seok on 2022/08/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/SwiftUI_Pinch_Pan_Zoom_GestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Pinch_Pan_Zoom_GestureApp.swift
+//  Shared
+//
+//  Created by park kyung seok on 2022/08/02.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Pinch_Pan_Zoom_GestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
